10e1c27f5055222f0f5a1b67ea2699b4ed3719a5
Felipe <felhenao@gmail.com>
2021-02-09 15:02:20 -0500

    Added addUser and removeUser functions

--- src/index.js
+++ src/index.js
@@ -14,13 +14,16 @@ const publicDirectoryPath = path.join(__dirname, '../public' )
 app.use(express.static(publicDirectoryPath))
 
 io.on('connection', (socket) => {
-    
-    socket.emit('message', generateMessage('Welcome!'))//emits to single client when new client connects 
-    socket.broadcast.emit('message', generateMessage('A new user has joined'))//emits to all (except current) clients
+    socket.on('join', ({ username, room }) => {
+        socket.join(room)
+        socket.emit('message', generateMessage('Welcome!'))//emits to single client when new client connects 
+        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`))//emits to all (except current) clients
+        //socket.emit, io.emit, socket.broadcast.emit, io.to.emit, socket.broadcast.to.emit
+    })
 
     socket.on('sendMessage', (message, callback) => {
-        io.emit('message', generateMessage(message))  //emit to all clients
-        callback('Delivered')
+        io.to('Pair Programming').emit('message', generateMessage(message))  //emit to all clients
+        callback()
     })
 
     socket.on('sendLocation', (coords, callback) => {
