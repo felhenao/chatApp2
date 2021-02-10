136e4e2e82f6ee8a5fb1965cb67b7737c940f6b5
Felipe <felhenao@gmail.com>
2021-02-10 11:54:02 -0500

    Added notification when users leave and join

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
 const { generateMessage, generateLocationMessage } = require('./utils/messages')
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')
 
 const app = express()
 const server = http.createServer(app)
@@ -14,25 +15,41 @@ const publicDirectoryPath = path.join(__dirname, '../public' )
 app.use(express.static(publicDirectoryPath))
 
 io.on('connection', (socket) => {
-    socket.on('join', ({ username, room }) => {
-        socket.join(room)
+    console.log('New WebSocket connection')
+
+    socket.on('join', (options, callback) => {
+        const { error, user } = addUser({ id: socket.id, ...options  })
+
+        if (error) {
+            return callback(error)
+        }
+
+        socket.join(user.room)
         socket.emit('message', generateMessage('Welcome!'))//emits to single client when new client connects 
-        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`))//emits to all (except current) clients
-        //socket.emit, io.emit, socket.broadcast.emit, io.to.emit, socket.broadcast.to.emit
+        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`))//emits to all (except current) clients
+
+        callback()
+
     })
 
     socket.on('sendMessage', (message, callback) => {
-        io.to('Pair Programming').emit('message', generateMessage(message))  //emit to all clients
+        const user = getUser(socket.id)
+
+        io.to(user.room).emit('message', generateMessage(user.username, message))  //emit to all clients
         callback()
     })
 
     socket.on('sendLocation', (coords, callback) => {
-        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`) )
+        io.emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`) )
         callback()
     })
 
     socket.on('disconnect', () => {
-        io.emit('message', generateMessage('A user has left!'))
+        const user = removeUser(socket.id)
+
+        if (user) {
+            io.to(user.room).emit('message', generateMessage(`${user.username} has left`))
+        }
     })
 })
 
